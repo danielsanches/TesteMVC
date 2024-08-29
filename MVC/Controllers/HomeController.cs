4462b9a316d505115a23856729821abd97c678e0
imagetech <imagetech@DESKTOP-N5IN9H2>
2024-08-29 14:49:17 -0400

    criande grid

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using MVC.Model.Home;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
+        public static List<DadosPessoaisModel> listaDados = new List<DadosPessoaisModel>();
         public HomeController() : base() { }
 
         public ActionResult Index()
         {
-            return View();
+            return View(new DadosPessoaisModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(DadosPessoaisModel dadosPessoaisModel)
+        {
+            dadosPessoaisModel.Id = listaDados.Count + 1;
+            listaDados.Add(dadosPessoaisModel);
+            return View(new DadosPessoaisModel { Itens = listaDados });
+        }
+        [HttpPost]
+        public ActionResult Remover()
+        {
+            return View(new DadosPessoaisModel());
         }
     }
+
 }
---
+++ MVC/Model/Home/DadosPessoaisModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Model.Home
+{
+    public class DadosPessoaisModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public List<DadosPessoaisModel> Itens { get; set; } = new List<DadosPessoaisModel>();
+    }
+}
